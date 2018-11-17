71e8b45f3c8feda0a706ca68b9f4d33c77ed00ff
Karl Watson <karlmwatson@outlook.com>
2018-11-17 20:51:46 +0000

    Add initial DOM Selectors

--- designs.js
+++ designs.js
@@ -3,6 +3,9 @@
 
 // When size is submitted by the user, call makeGrid()
 
+let submitButton = $("#submitButton");
+let colorPickerButton = $("#colorPicker");
+
 function makeGrid() {
 
 // Your code goes here!
