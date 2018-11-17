6f29d89612e1e89deadb12a915fafd8479325ea1
Karl Watson <karlmwatson@outlook.com>
2018-11-17 22:44:04 +0000

    Created makeGrid function to create a basic table

--- designs.js
+++ designs.js
@@ -3,11 +3,23 @@
 
 // When size is submitted by the user, call makeGrid()
 
-let submitButton = $("#submitButton");
-let colorPickerButton = $("#colorPicker");
+const submitButton = $("#submitButton");
+const colorPickerButton = $("#colorPicker");
+let inputHeight = $("#inputHeight");
+let inputWidth = $("#inputWidth");
+let tableDraw = $("#pixelCanvas");
 
 function makeGrid() {
 
-// Your code goes here!
+  for (let i = 0; i < inputHeight; i++) {
+    $(tableDraw).append( `<tr class = "Row${i}" id = "temp">`);
+    let temp = $(`.Row${i}`);
+    for (let i = 0; i < inputWidth; i++) {
+      $(temp).append( `<td class = "Column${i}">&nbsp;</td>` );
+      }
+    }
 
-}
+  // Your code goes here!
+
+  }
+// Your code goes here!
